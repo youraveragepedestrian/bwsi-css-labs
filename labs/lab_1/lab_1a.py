7431f7d82cdb1f625b356da539ca5b1183b939bb
Isha Patil <126841205+youraveragepedestrian@users.noreply.github.com>
2026-02-22 16:01:10 -0500

    speed change pt2 lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -15,4 +15,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-#This is to simulate a change made on a robot: robot_speed = 5 # m/s
+#This is to simulate a change made on a robot: robot_speed = 3 # m/s
